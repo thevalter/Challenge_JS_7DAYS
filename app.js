db4e0a48a9f8269a0675bfcf09ccd790825e0887
nbx7 <101077340+nbx7@users.noreply.github.com>
2022-07-02 10:43:48 -0300

    first day

---
+++ app.js
@@ -0,0 +1,8 @@
+let name = document.getElementById('name');
+let birthDate = document.getElementById('birth-date');
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(name.value);
+    console.log(birthDate.value);
+});
