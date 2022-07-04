110c3287e4ee05b300a1cc814a806135b8b96ecb
nbx7 <101077340+nbx7@users.noreply.github.com>
2022-07-04 09:52:03 -0300

    third day

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-let name = document.getElementById('name');
-let birthDate = document.getElementById('birth-date');
 
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(name.value);
-    console.log(birthDate.value.split('-').reverse().join('/'));
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_user')) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("db_user", JSON.stringify(dbClient));
+
+const readClient = () => getLocalStorage();
+
+const createClient = (saveClient) => {
+    const dbClient = getLocalStorage();
+    dbClient.push(saveClient);
+    setLocalStorage(dbClient);
+};
+
+document.querySelector('form').addEventListener('submit', () => {
+    const saveClient = {
+        nome: document.getElementById('name').value,
+        nascimento: document.getElementById('birth-date').value.split('-').reverse().join('/')
+    };
+    createClient(saveClient);
 });
+
+const createRow = (saveClient) => {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td class="rowInfo">${saveClient.nome}</td>
+        <td class="rowInfo">${saveClient.nascimento}</td>
+    `
+    document.querySelector('table>tbody').appendChild(newRow);
+};
+
+const updateTable = () => {
+    const dbClient = readClient();
+    dbClient.forEach(createRow);
+}
+
+updateTable();
